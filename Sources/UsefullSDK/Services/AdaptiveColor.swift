74089480431824a5917b10cae3a471c93d0431a8
burak.uzunboy <b_uzunboy@outlook.com>
2020-01-18 20:47:51 +0100

    Adaptive color implementation

---
+++ Sources/UsefullSDK/Services/AdaptiveColor.swift
@@ -0,0 +1,48 @@
+//
+//  AdaptiveColor.swift
+//  
+//
+//  Created by Burak Uzunboy on 18.01.20.
+//
+
+import UIKit
+
+/// Adaptive Color that adapts given color according to the screen brightness changes.
+@objc public class AdaptiveColor: NSObject {
+    
+    var hue: CGFloat, brightness: CGFloat, saturation: CGFloat, alpha: CGFloat
+    private var callback: ColorUpdateCallback
+    
+    public typealias ColorUpdateCallback = ((_ color: UIColor)->())
+    
+    /// Cannot be used
+    private override init() {
+        fatalError("Use init:color instead")
+    }
+    
+    /// Initializes color with the given `UIColor`.
+    /// - Parameters:
+    ///   - color: Base color for the object
+    ///   - callback: Will be called when any screen brightness changes notified.
+    @objc public init(color: UIColor, callback: @escaping ColorUpdateCallback) {
+        self.hue = 0
+        self.brightness = 0
+        self.saturation = 0
+        self.alpha = 0
+        color.getHue(&hue, saturation: &saturation,
+                     brightness: &brightness, alpha: &alpha)
+        self.callback = callback
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(didScreenBrightnessChanged),
+                                               name: UIScreen.brightnessDidChangeNotification, object: nil)
+        self.didScreenBrightnessChanged()
+    }
+
+    @objc private func didScreenBrightnessChanged() {
+        let screenBrightness = UIScreen.main.brightness
+        
+        let newColor = UIColor(hue: self.hue, saturation: self.saturation, brightness: screenBrightness, alpha: self.alpha)
+        self.callback(newColor)
+    }
+    
+}
